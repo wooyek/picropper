9b3c3e11d13246a275c00ada35b56e2b1eef98ba
Janusz Skonieczny <git.js@bravelabs.pl>
2018-06-29 22:26:04 +0200

    poc

--- src/picropper/views.py
+++ src/picropper/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import resolve_url
 from django_powerbank.views.auth import AbstractAuthorizedView, AuthenticatedView
+from filer.models import Image
 from pascal_templates import ListView
 from pascal_templates.views import CreateView, DetailView
 
@@ -8,28 +9,28 @@ from . import models
 
 
 class ImageList(AuthenticatedView, ListView):
-    model = models.Image
+    template_name = "picropper/Image/list.html"
+    model = Image
     paginate_by = 50
 
 
 class ImageCreate(AuthenticatedView, CreateView):
-    model = models.Image
+    model = Image
     fields = ('name',)
 
     def get_success_url(self):
         return resolve_url("picropper:SampleDetail", self.object.pk)
 
 
-class ImageDetail(AbstractAuthorizedView, DetailView):
-    model = models.Image
-
-    def is_authorized(self, *args, **kwargs):
-        return self.get_object().user.pk == self.request.user.pk
+class ImageDetail(AuthenticatedView, DetailView):
+    template_name = "picropper/Image/detail.html"
+    model = Image
 
     def get_context_data(self, **kwargs):
         from filer.models import ThumbnailOption
         options = ThumbnailOption.objects.all()
-        thumbnailer = self.object.file.easy_thumbnails_thumbnailer
+        # thumbnailer = self.object.file.easy_thumbnails_thumbnailer
+        thumbnailer = self.object.easy_thumbnails_thumbnailer
         versions = [(option.name, thumbnailer.get_thumbnail(option.as_dict)) for option in options]
         return super().get_context_data(versions=versions, **kwargs)
 
